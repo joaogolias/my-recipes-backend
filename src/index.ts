d09fc0080315250d75717e820a715bf36bf9dd74
Golias <joao.victor@mobile2you.com.br>
2020-05-17 20:07:59 -0300

    done a lot of setups

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,49 @@
-import express, { Request, Response } from "express";
-import { AddressInfo } from "net";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
+import { Routes } from "./routes";
+import cors from "cors";
 
-const app = express();
+createConnection()
+  .then(async (connection) => {
+    // create express app
+    const app = express();
+    app.use(cors());
+    app.use(bodyParser.json());
 
-app.use(express.json());
+    // register express routes from defined application routes
+    Routes.forEach((route) => {
+      (app as any)[route.method](
+        route.route,
+        (req: Request, res: Response, next: Function) => {
+          const result = new (route.controller as any)()[route.action](
+            req,
+            res,
+            next
+          );
+          if (result instanceof Promise) {
+            result.then((result) =>
+              result !== null && result !== undefined
+                ? res.send(result)
+                : undefined
+            );
+          } else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
+        }
+      );
+    });
 
-app.get("/", (_, res: Response) => {
-  res.status(200).send({
-    message: "helloworld",
-  });
-});
+    // setup express app here
+    // ...
 
-export default app;
+    // start express server
+    app.listen(3000);
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  if (server) {
-    const address = server.address() as AddressInfo;
-    console.log(`Server running on http://localhost:${address.port}`);
-  } else {
-    console.log("Failed to load server");
-  }
-});
+    console.log(
+      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
+    );
+  })
+  .catch((error) => console.log(error));
