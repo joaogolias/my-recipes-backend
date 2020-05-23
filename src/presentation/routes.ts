acb863e695e23927a8e1b0d85cba38be179578f8
Golias <joao.victor@mobile2you.com.br>
2020-05-23 18:38:55 -0300

    finished auth0 verify token POC

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -5,11 +5,19 @@ import { getById } from "./users/getById";
 import { all } from "./users/all";
 import { save } from "./users/save";
 import { deleteUser } from "./users/delete";
+import { checkJwt } from "./auth";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get("/token/verify", checkJwt(), (req, res) => {
+  //Note: user information can be retrived by const user = (req as any).user
+  res.send({
+    msg: "Your Access Token was successfully validated!",
+  });
+});
 
 app.get("/users", all);
 app.get("/users/:id", getById);
