6ae6c7a3f1910c350b7282fedcc3d666021fdad8
Golias <joao.victor@mobile2you.com.br>
2020-07-10 12:44:22 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 createConnection()
   .then(async (connection) => {
-    const server = app.listen(process.env.PORT || 3000, () => {
+    const server = app.listen(process.env.PORT || 3001, () => {
       if (server) {
         console.log(
           `Server running on http://localhost:${(server.address() as any).port}`
